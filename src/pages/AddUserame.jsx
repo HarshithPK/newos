5bc99c51534e5a59cc81ec745b6c997a93b96b36
Harshith P K <hp740942@gmIL.com>
2021-03-19 23:06:38 +0530

    Twenty Fourth Commit

--- src/pages/AddUserame.jsx
+++ src/pages/AddUserame.jsx
@@ -30,7 +30,7 @@ export default function AddUsername() {
             promises.push(updateUsername(usernameRef.current.value));
         }
 
-        //Webhook call to MongoDB Realm to add the user's username and an empty array called favouriteAstroids Mongo Database
+        //Webhook call to MongoDB Realm to add the user's username and an empty array called favouriteAsteroids Mongo Database
         Promise.all(promises)
             .then(async () => {
                 const user = {
